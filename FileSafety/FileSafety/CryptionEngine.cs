fbda9b603e7a8673618d29277df741ba589979a5
Bo Wan <sean.b.wan@gmail.com>
2015-03-15 11:18:30 -0700

    sub ver 1.0

---
+++ FileSafety/FileSafety/CryptionEngine.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FileSafety
+{
+    public static class CryptionEngine
+    {
+        public static IEnumerable<double> EncryptFile(this FileInfo fileInfo, string password)
+        {
+            FileStream fsCrypt = null;
+            FileStream fsIn = null;
+            CryptoStream cs = null;
+            RijndaelManaged RMCrypto = null;
+            try
+            {
+                
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] temp = UE.GetBytes(password).Where(p => p!= 0).Select(a =>a).ToArray();
+                byte[] key = UE.GetBytes("****************").Where(p => p != 0).Select(a => a).ToArray();
+                if (temp.Length > 16) throw new ArgumentException("Password can't be more than 16 characters");
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    key[i] = temp[i];
+                }
+
+                string cryptFile = fileInfo.FullName+".cryp";
+                fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                RMCrypto = new RijndaelManaged();
+                RMCrypto.Key = key;
+                RMCrypto.IV = key;
+                ICryptoTransform transform = RMCrypto.CreateEncryptor();
+                cs = new CryptoStream(fsCrypt,
+                    transform,
+                    CryptoStreamMode.Write);
+
+                fsIn = fileInfo.Open(FileMode.Open);
+                var count = 1;
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                {
+                    cs.WriteByte((byte)data);
+                    if (count % (fsIn.Length / 100) == 0) 
+                        yield return count / (fsIn.Length / 100);
+                    count++;
+                }
+                    
+            }
+            finally
+            {
+                if (fsIn != null) fsIn.Close();
+                if (cs != null) cs.Close();
+                if (fsCrypt != null) fsCrypt.Close();
+                if (RMCrypto != null) RMCrypto.Dispose();
+            }
+        }
+
+        public static IEnumerable<double> DecryptFile(this FileInfo fileInfo, string password)
+        {
+            FileStream fsCrypt = null;
+            FileStream fsOut = null;
+            CryptoStream cs = null;
+            RijndaelManaged RMCrypto = null;
+            try
+            {
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] temp = UE.GetBytes(password).Where(p => p != 0).Select(a => a).ToArray();
+                byte[] key = UE.GetBytes("****************").Where(p => p != 0).Select(a => a).ToArray();
+                if (temp.Length > 16) throw new ArgumentException("Password can't be more than 16 characters");
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    key[i] = temp[i];
+                }
+
+
+                fsCrypt = fileInfo.Open(FileMode.Open);
+
+                RMCrypto = new RijndaelManaged();
+
+                cs = new CryptoStream(fsCrypt,
+                   RMCrypto.CreateDecryptor(key, key),
+                   CryptoStreamMode.Read);
+
+                var outputFile = fileInfo.FullName.Remove(fileInfo.FullName.Length - 4);
+                fsOut = new FileStream(outputFile, FileMode.Create);
+
+                var count = 1;
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                {
+                    fsOut.WriteByte((byte)data);
+                    if (count % (fsCrypt.Length / 100) == 0)
+                        yield return count / (fsCrypt.Length / 100);
+                    count++;
+                }
+            }
+            finally
+            {
+                if (fsOut != null) fsOut.Close();
+                if (cs != null) cs.Close();
+                if (fsCrypt != null) fsCrypt.Close();
+                if (RMCrypto != null) RMCrypto.Dispose();
+            }
+        }
+    }
+}
---
+++ FileSafety/FileSafety/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileSafety
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileName = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+            passwordbox1.PasswordChanged += (sender, args) =>
+            {
+                encryptBtn.IsEnabled = !String.IsNullOrEmpty(passwordbox1.Password) && File.Exists(fileName);
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                fileName = dialog.FileName;
+                decryptBtn.IsEnabled = File.Exists(fileName) && dialog.SafeFileName.Contains(".cryp");
+                (sender as Button).Content = dialog.SafeFileName;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var target = new FileInfo(fileName);
+            if (passwordbox1.Password != passwordbox2.Password) MessageBox.Show("The passwords are not same! Please try again.","Error");
+            else if (target.Exists && !string.IsNullOrEmpty(passwordbox1.Password))
+            {
+                prgGrid.Visibility = System.Windows.Visibility.Visible;
+                mainGrid.Opacity = 0.3;
+                mainGrid.IsEnabled = false;
+                Task.Factory.StartNew(() => 
+                {
+                    try
+                    {
+                        foreach (var percentage in target.EncryptFile(passwordbox1.Password))
+                        {
+                            Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                prgbar.Value = percentage;
+                                prgText.Text = string.Format("Encrypeted {0}%", percentage);
+                            }), null);
+                        }
+                        Dispatcher.BeginInvoke(new Action(
+                            () => prgGrid.Visibility = System.Windows.Visibility.Collapsed
+                            ));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(string.Format("{0}/n/n{1}/{2}", "Encryption failed!", ex.Message, ex.StackTrace)
+                        , "Error");
+                    }
+                });
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var target = new FileInfo(fileName);
+            if (target.Exists && target.Name.Contains(".cryp") && !string.IsNullOrEmpty(passwordbox1.Password))
+            {
+                prgGrid.Visibility = System.Windows.Visibility.Visible;
+                mainGrid.Opacity = 0.3;
+                mainGrid.IsEnabled = false;
+                Task.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        foreach (var percentage in target.DecryptFile(passwordbox1.Password))
+                        {
+                            Dispatcher.BeginInvoke(new Action(() =>
+                            {
+                                prgbar.Value = percentage;
+                                prgText.Text = string.Format("Encrypeted {0}%", percentage);
+                            }), null);
+                        }
+                        Dispatcher.BeginInvoke(new Action(
+                            () => prgGrid.Visibility = System.Windows.Visibility.Collapsed
+                            ));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(string.Format("{0}/n/n{1}/{2}", "Decryption failed!", ex.Message, ex.StackTrace)
+                        , "Error");
+                    }
+                });
+            }
+        }
+    }
+}
